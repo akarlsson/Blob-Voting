acf16fdda97354d2bf0b73e931a0386a9433ddde
Andreas Karlsson <ak@allmostfamous.at>
2013-05-24 09:22:32 +0200

    changed to show authors name and email address

--- app/views/items/index.blade.php
+++ app/views/items/index.blade.php
@@ -13,7 +13,14 @@
                 <div class="item">
                     <h3>{{ $item->name }}</h3>
 					<p><img src="{{ Request::root() }}/photos/{{ $item->image }}" width="200" style="float:left;margin-right:10px;" />{{ Str::limit($item->body, 150) }}</p>
-					<p><strong>Eingetragen von:</strong> {{ $item->author }}</p>
+					<p>
+                    <?php
+					//$sender = json_decode(file_get_contents('http://graph.facebook.com/'.$item->author))->name;
+					//$senderemail = json_decode(file_get_contents('http://graph.facebook.com/'.$item->author))->email;
+					$fbuser = User::where("fbid","=",$item->author)->first();
+					echo '<p  class="subline">Eingeschickt von <a href="http://facebook.com/'.$item->author.'" target="_blank">'.$fbuser->username.'</a><br />'.$fbuser->email.'</p>';
+					?>
+                    </p>
                     <p>
                      {{ Form::open(array('method' => 'DELETE', 'route' => array('items.destroy', $item->id),'style' => 'display:inline;')) }}{{ link_to_route('items.edit', 'Bearbeiten', array($item->id), array('class' => 'btn btn-info')) }}
                             {{ Form::submit('Löschen', array('class' => 'btn btn-danger')) }}
